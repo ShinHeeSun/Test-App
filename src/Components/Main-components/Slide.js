a4e6c8e3b6642dcc36c12037a104231ceaf16a1d
ShinHeeSun <sbs2110@nate.com>
2022-03-30 16:55:57 +0900

    new commit

--- src/Components/Main-components/Slide.js
+++ src/Components/Main-components/Slide.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Dhlqn from '../../img/외부.jpg';
+import Soqn from '../../img/내부.jpg';
+import '../../App.css';
 
-function Slide () {
-return (
-    <div>
-        
-    </div>
-);
+function Slide() {
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <h3>
+          <img src={Dhlqn} className="img" />
+        </h3>
+      </div>
+      <div>
+        <h3>
+          <img src={Soqn} />
+        </h3>
+      </div>
+      <div>
+        <h3>
+          <img src={Dhlqn} />
+        </h3>
+      </div>
+      <div>
+        <h3>
+          <img src={Dhlqn} />
+        </h3>
+      </div>
+      <div>
+        <h3>
+          <img src={Dhlqn} />
+        </h3>
+      </div>
+      <div>
+        <h3>
+          <img src={Dhlqn} />
+        </h3>
+      </div>
+    </Slider>
+  );
 }
 export default Slide;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Main-components/Header';
 import Nav from './Main-components/Nav';
 import Slide from './Main-components/Slide';
+
 function Main() {
   return (
     <div>
